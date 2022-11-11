834299c194195aa30ca197dc67e83aae6453ce75
B221881-2022 <anon@anon.com>
2022-11-11 20:46:07 +0000

    Bare bones of returning fasta file of protein sequences of user interest

--- script1.py
+++ script1.py
@@ -3,6 +3,25 @@ import os, sys, subprocess
 import numpy as np
 import pandas as pd
 
-subprocess.call('echo "welcome to the ICA2"', shell=True)
-subprocess.call("esearch -db protein -query 'pyruvate dehydrogenase[PROT]'|\
-efilter -organism 'ascomycete fungi'|efetch -format fasta>file1", shell=True)
+print('''Welcome to ICA2
+---
+''')
+
+#Getting user input so they can specify the proteins and organisms they
+#are interested in
+
+#protein_family=raw_input("What protein family are you interested in?")
+#taxonomic_subset=raw_input("What taxonomic subset do you wish to investigate?")
+
+protein_family = 'pyruvate dehydrogenase'
+taxonomic_subset='ascomycete fungi'
+
+
+#using esearch efilter and efetch to return a fasta file containing sequences
+#in the protein family and the taxonomic subset
+cmd =f'esearch -db protein -query "{protein_family}[PROT]"|efilter -organism "{taxonomic_subset}"\
+|efetch -format fasta>file1'
+
+subprocess.call(cmd, shell=True)
+
+os.system("head file1")
