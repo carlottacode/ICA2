d67d3b62dbd5bc63a1983d2a61a3c1a238c83742
B221881-2022 <anon@anon.com>
2022-11-12 00:20:09 +0000

    Started error trapping protein family and taxonomic subset user input

--- script1.py
+++ script1.py
@@ -10,18 +10,31 @@ print('''Welcome to ICA2
 #Getting user input so they can specify the proteins and organisms they
 #are interested in
 
-#protein_family=raw_input("What protein family are you interested in?")
-#taxonomic_subset=raw_input("What taxonomic subset do you wish to investigate?")
+try:
+	protein_family=input("What protein family are you interested in?")
+	taxonomic_subset=input("What taxonomic subset do you wish to investigate?")
 
-protein_family = 'pyruvate dehydrogenase'
-taxonomic_subset='ascomycete fungi'
+	#protein_family = 'pyruvate dehydrogenase'
+	#taxonomic_subset='ascomycete fungi'
+	esearch_cmd = f'esearch -db protein -query "{protein_family}[PROT]" | grep "Count"'
+	line =  subprocess.check_output(esearch_cmd, shell=True).decode("utf-8")
 
 
-#using esearch efilter and efetch to return a fasta file containing sequences
-#in the protein family and the taxonomic subset
-cmd =f'esearch -db protein -query "{protein_family}[PROT]"|efilter -organism "{taxonomic_subset}"\
-|efetch -format fasta>file1'
+	if int(line[9:-9]) > 0 :
 
-subprocess.call(cmd, shell=True)
 
-os.system("head file1")
+		#using esearch efilter and efetch to return a fasta file containing sequences
+		#in the protein family and the taxonomic subset
+		cmd =f'esearch -db protein -query "{protein_family}[PROT]"|efilter -organism "{taxonomic_subset}"\
+		|efetch -format fasta>file1'
+
+		subprocess.check_call(cmd, shell=True)
+		try:
+			subprocess.call(cmd, shell=True)
+			os.system("head file1")
+		except:
+			print('idk what the issue is')
+except:
+	print("your input wasn't good babe")
+finally:
+	print("That's it!")
